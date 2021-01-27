b714dad9dcd28fc295ef9b51c87b63ba56050460
angiegarrote <78098567+angiegarrote@users.noreply.github.com>
2021-01-27 14:41:27 -0300

    Add files via upload

---
+++ makecachematrix.R
@@ -0,0 +1,32 @@
+makeCacheMatrix <- function(x = matrix()){
+        inversa <- NULL
+        set <- function(y){
+                x<<-y
+                inversa <- NULL
+        }
+        get <- function(){x}
+        setInversa <- function(inversacalculada){inversa<<- inversacalculada}
+        getInversa <- function(){inversa}
+        list(set = set, get = get,
+             setInversa = setInversa,
+             getInversa = getInversa) 
+        
+}
+
+
+
+
+
+
+cacheSolve <- function(x, ...) {
+        inversa <-x$getInversa()
+        if(!is.null(inversa)) {
+                message("getting cached data")
+                return(inversa)}
+        data <- x$get()
+        inversa <- solve(data, ...)
+        x$setInversa(inversa)
+        inversa
+}
+
+
